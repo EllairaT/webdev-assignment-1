5181afaaf93aefbf8363f5afcd71eeb942d2a554
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-03-24 02:19:39 +1300

    new look

--- assign1/poststatusform.php
+++ assign1/poststatusform.php
@@ -4,7 +4,7 @@
 
 <!DOCTYPE html>
 <html>
-<div class="content">
+<div class="content center-block text-center">
 
     <head>
         <meta charset="utf-8">
@@ -15,41 +15,62 @@
     </head>
 
     <body>
-        <div class="container-fluid">
-            <div id="main-cont">
-                <h1>Budget Facebook</h1>
+        <div class="container">
+            <div class="jumbotron">
+                <h1 class="display-4">Budget Facebook</h1>
+                <p class="lead">Or is it budget twitter? They're pretty much the same thing.</p>
+                <hr class="my-4">
+            </div>
+          
+            <div class="card main-card text-md-start" style="width: 50%;">
+                <div class="card-body">
 
-                <!--Asks user to input a status code. The S is added for the user.-->
-                <div class="form-group">
-                    <label for="statcode">Status Code</label>
-
-                    <div class="input-group mb-3">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="sc">S</span>
+                    <!--Status Code-->
+                    <div class="form-group">
+                        <label for="statcode">Status Code</label>
+                        <div class="input-group mb-1">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="sc">S</span>
+                            </div>
+                            <input type="text" name="statuscode" id="statcode" maxlength="4" pattern="[0-9]{4}" size=3/>
                         </div>
-                        <input type="text" name="statuscode" id="statcode" maxlength="4" pattern="[0-9]{4}" size=3 />
+                        <small id="passwordHelpBlock" class="form-text text-muted">Your status code must be a unique number e.g. 0001.</small>
                     </div>
-                </div>
 
-                <!--input field accepts alphanumerical numbers and comma, space, period, exclamation point and question marks -->
-                <div class="form-group">
-                    <label for="stattext">Status</label>
-                    <div class="input-group mb-3">
-                        <input type="text" name="status" id="stattext" pattern="[a-zA-Z0-9 ,.!\?]" />
+                    <!--Status field -->
+                    <div class="form-group" id="stat">
+                        <div class="input-group form-group">
+                            <input type="text" class="form-control mb-3 mt-3" name="status" id="statustext" pattern="[a-zA-Z0-9 ,.!\?]" placeholder="Got something to say?" />
+                        </div>
                     </div>
-                </div>
 
-                <div class="form-group">
-                    <label>Share:</label>
+                    <!--Visibility-->
+                    <div class="form-group mb-3">
+                        <label>Share:</label>
 
-                    <label for="public">Public</label>
-                    <input type="radio" name="visibility" id="public" value="public" />
+                        <label for="public">Public</label>
+                        <input type="radio" name="visibility" id="public" value="public" />
 
-                    <label for="friends">Friends</label>
-                    <input type="radio" name="visibility" id="friends" value="friends" />
+                        <label for="friends">Friends</label>
+                        <input type="radio" name="visibility" id="friends" value="friends" />
 
-                    <label for="me">Only Me</label>
-                    <input type="radio" name="visibility" id="me" value="me" />
+                        <label for="me">Only Me</label>
+                        <input type="radio" name="visibility" id="me" value="me" />
+                    </div>
+
+                    <!--Date-->
+                    <div class="form-group mb-3">
+                        <label>Date</label>
+                        <input type="date" name="Date" id="postdate" />
+                    </div>
+
+                    <!--Permissions-->
+                    <div class="form-group mb-3">
+                        <label>Permissions</label>
+                        <input type="checkbox" name="permission" value="like">Allow Like
+                        <input type="checkbox" name="permission" value="comment">Allow Comments
+                        <input type="checkbox" name="permission" value="share">Allow Share
+                    </div>
                 </div>
             </div>
         </div>
