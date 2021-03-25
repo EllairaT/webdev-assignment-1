405bf65c56f65ce4616d11826e0a91ec038b3068
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-03-25 16:44:25 +1300

    styled checkboxes

--- assign1/poststatusform.php
+++ assign1/poststatusform.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -15,28 +15,37 @@
     </head>
 
     <body>
+        <!-- Header -->
         <div class="container">
             <div class="jumbotron">
-                <h1 class="display-4">Budget Facebook</h1>
-                <p class="lead">Or is it budget twitter? They're pretty much the same thing.</p>
+                <h1 class="display-4">Simpbook</h1>
+                <p class="lead">For simps, by simps.</p>
                 <hr class="my-4">
             </div>
-          
-            <div class="card main-card text-md-start" style="width: 50%;">
-                <div class="card-body">
 
-                    <!--Status Code-->
-                    <div class="form-group">
-                        <label for="statcode">Status Code</label>
+            <!-- Start of card -->
+            <div class="card main-card text-md-start" style="width: 50%;">
+                <!--Card Header contains status code and date -->
+                <!--Status Code-->
+                <div class="card-header row">
+                    <div class="form-group col-md">
                         <div class="input-group mb-1">
                             <div class="input-group-prepend">
                                 <span class="input-group-text" id="sc">S</span>
                             </div>
-                            <input type="text" name="statuscode" id="statcode" maxlength="4" pattern="[0-9]{4}" size=3/>
+                            <input type="text" class="input-special" name="statuscode" id="statcode" maxlength="4" pattern="[0-9]{4}" size="3" />
                         </div>
                         <small id="passwordHelpBlock" class="form-text text-muted">Your status code must be a unique number e.g. 0001.</small>
                     </div>
 
+                    <!--Date-->
+                    <div class="form-group mb-3 col-md float-right">
+                        <input type="date" name="Date" id="postdate" />
+                    </div>
+                </div>
+
+                <!-- Start of card body -->
+                <div class="card-body">
                     <!--Status field -->
                     <div class="form-group" id="stat">
                         <div class="input-group form-group">
@@ -58,21 +67,37 @@
                         <input type="radio" name="visibility" id="me" value="me" />
                     </div>
 
-                    <!--Date-->
-                    <div class="form-group mb-3">
-                        <label>Date</label>
-                        <input type="date" name="Date" id="postdate" />
-                    </div>
 
-                    <!--Permissions-->
-                    <div class="form-group mb-3">
-                        <label>Permissions</label>
+
+                    <!--Permissions version 1 (default checkbox look)
+                    
+                        <div class="form-group mb-3">
+                        <label>Permissions:</label>
                         <input type="checkbox" name="permission" value="like">Allow Like
                         <input type="checkbox" name="permission" value="comment">Allow Comments
                         <input type="checkbox" name="permission" value="share">Allow Share
+
+                    </div> -->
+
+                    <!-- permissions version 2 (checkbox buttons)-->
+                    <div class="btn-group permissions-group d-flex" role="group" aria-label="Basic checkbox toggle button group">
+                        <input type="checkbox" class="btn-check" id="btnlike" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="btnlike">Allow Like</label>
+
+                        <input type="checkbox" class="btn-check" id="btncomment" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="btncomment">Allow Comments</label>
+
+                        <input type="checkbox" class="btn-check" id="btnshare" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="btnshare">Allow Share</label>
+                    </div>
+
+                    <!--Submit button -->
+                    <div class="d-grid gap-2 mt-2">
+                        <button type="button" class="btn btn-primary btn-submit" value="Submit">Post</button>
                     </div>
                 </div>
             </div>
+            <!-- End of card -->
         </div>
     </body>
 </div>
