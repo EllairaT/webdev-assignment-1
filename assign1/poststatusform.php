2c2987e1ce5d4bdada5480dabc1c525aefca11f7
Ellaira <62170666+EllairaT@users.noreply.github.com>
2021-04-06 23:48:42 +1200

    new look

--- assign1/poststatusform.php
+++ assign1/poststatusform.php
@@ -10,33 +10,32 @@ date_default_timezone_set('Pacific/Auckland');
 
 <!DOCTYPE html>
 <html>
-<div class="content center-block text-center">
 
-    <head>
-        <title>Post Status</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="js/script.js"></script>
-        <link rel="stylesheet" href="scss/style.css" type="text/css">
+<head>
+    <title>Post Status</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="scss/style.css" type="text/css">
+    <script src="js/script.js"></script>
 
-        <script type="text/javascript">
-            $(document).ready(function() {
-                initializePoppers();
+    <script type="text/javascript">
+        $(document).ready(function() {
+            initializePoppers();
+        });
+    </script>
+</head>
 
-
-            });
-        </script>
-    </head>
-
-    <body>
+<body>
+    <div class="content center-block text-center">
         <!-- Header -->
         <div class="container">
             <div class="jumbotron">
-                <h1 class="display-4">pls no more php</h1>
-                <p class="lead">it's so bad.</p>
+                <h1 class="display-4">whispurr</h1>
+                <p class="lead">keep meowing</p>
                 <hr class="my-4">
             </div>
             <form action="poststatusprocess.php" method="POST">
@@ -46,20 +45,20 @@ date_default_timezone_set('Pacific/Auckland');
 
                     <div class="card-header navbar">
                         <!-- Status Code -->
-                        <div class="d-inline statcode post-form">
-                            <div class="input-group w-25 mx-2">
+                        <div class="d-inline statcode">
+                            <div class="input-group mx-2">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text float-end px-0" id="sc">S</span>
                                 </div>
-                                <input type="text" class="form-control px-1 w-25" name="statuscode" maxlength="4" pattern="[0-9]{4}" placeholder="0000" data-bs-toggle="popover" title="Status Code" data-bs-content="Kinda like a diary entry, make a unique 4 digit number! (e.g. 0123)" data-bs-trigger="hover" />
+                                <input type="text" class="form-inline px-1 w-25" name="statuscode" maxlength="4" pattern="[0-9]{4}" placeholder=" 0000" data-bs-toggle="popover" title="Status Code" data-bs-content="Kinda like a diary entry, make a unique 4 digit number! (e.g. 0123)" data-bs-trigger="hover" autocomplete="off" />
                             </div>
 
                         </div>
 
                         <!--Date-->
-                        <div class="form-inline w-25 post-form">
+                        <div class="form-inline w-25">
                             <div class="input-group">
-                                <input type="date" class="form-control px-0" name="date" id="postdate" value="<?php echo date('Y-m-d'); ?>" />
+                                <input type="date" class="form-control bg-transparent px-0" name="date" id="postdate" value="<?php echo date('Y-m-d'); ?>" />
                             </div>
                         </div>
                     </div>
@@ -74,7 +73,7 @@ date_default_timezone_set('Pacific/Auckland');
                                     Friends
                                 </button>
                                 <span class="d-inline-flex mx-3" data-bs-toggle="popover" title="Who can see this?" data-bs-content="By default, only your friends can see what you post." data-bs-trigger="hover">
-                                    <img src="..\node_modules\bootstrap-icons\icons\info-circle.svg" width="auto" height="auto">
+                                    <i class="bi bi-info-circle" style="font-size:20px; color: cornflowerblue; " width="auto" height="auto"></i>
                                 </span>
 
                                 <div class="dropdown-menu dropdown-menu-dark" aria-labelledby="sharedropdownmenu">
@@ -101,13 +100,13 @@ date_default_timezone_set('Pacific/Auckland');
                             <!-- Permission badges tell the user which permissions are enabled -->
                             <div class="d-inline">
                                 <div class="badge rounded-pill bg-warning text-dark float-end mx-1" id="share-badge" style="display: none;">
-                                    <img src="..\node_modules\bootstrap-icons\icons\share.svg" width="auto" height="auto">
+                                    <i class="bi bi-share text-light" style="font-size:20px;" width="auto" height="auto"></i>
                                 </div>
                                 <div class="badge rounded-pill bg-info float-end mx-1" id="comment-badge" style="display: none;">
-                                    <img src="..\node_modules\bootstrap-icons\icons\chat-square-dots.svg" width="auto" height="auto">
+                                    <i class="bi bi bi-chat-square-dots text-light" style="font-size:20px;" width="auto" height="auto"></i>
                                 </div>
                                 <div class="badge rounded-pill bg-danger float-end mx-1" id="like-badge" style="display: none;">
-                                    <img src="..\node_modules\bootstrap-icons\icons\heart.svg" width="auto" height="auto">
+                                    <i class="bi bi-heart text-light" style="font-size:20px;" width="auto" height="auto"></i>
                                 </div>
                             </div>
                         </div>
@@ -118,15 +117,15 @@ date_default_timezone_set('Pacific/Auckland');
                         <div>
                             <div class="form-group" id="stat-field">
                                 <div class="input-group form-group">
-                                    <input type="text" class="form-control mt-3" name="status" id="statustext" pattern="[a-zA-Z0-9 ,.!\?]" placeholder="Got something to say?" />
+                                    <input type="text" class="form-control mt-3" name="status" id="statustext" pattern="[A-Za-z0-9\(\)._\-‘\?!]+" placeholder="Got something to say?" autocomplete="off" />
                                 </div>
                             </div>
                         </div>
 
                         <!--collapse button -->
-                        <button class="btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapsePermissions" aria-expanded="false" aria-controls="collapsePermissions">
+                        <button class="btn " type="button" data-bs-toggle="collapse" data-bs-target="#collapsePermissions" aria-expanded="false" aria-controls="collapsePermissions">
                             <span><small class="text-muted" data-bs-toggle="popover" data-bs-trigger="hover" data-bs-content="You can choose if you want your audience to like, comment or even share your post!" title="Permissions!" data-bs-placement="left">Permissions
-                                    <img src="..\node_modules\bootstrap-icons\icons\caret-right.svg" width="auto" height="auto">
+                                    <i class="bi bi-caret-right" width="auto" height="auto"></i>
                                 </small></span>
                         </button>
 
@@ -137,21 +136,21 @@ date_default_timezone_set('Pacific/Auckland');
                                 <div>
                                     <div class="form-check form-check-inline" data-bs-toggle="tooltip" title="Like">
                                         <label>
-                                            <input class="form-check-input" type="checkbox" name="permission-like" value="like" id="likeCheck" onClick="addPermission('like')">
-                                            <img src="..\node_modules\bootstrap-icons\icons\heart-fill.svg" width="auto" height="auto"></label>
+                                            <input class="form-check-input" type="checkbox" name="permissions[]" value="like" id="likeCheck" onClick="addPermission('like')">
+                                            <i class="bi bi-heart-fill text-danger" style="font-size:20px;" width="auto" height="auto"></i>
                                     </div>
 
                                     <div class="form-check form-check-inline" data-bs-toggle="tooltip" title="Comment">
                                         <label>
-                                            <input class="form-check-input" type="checkbox" name="permission-comment" value="comment" id="commentCheck" onClick="addPermission('comment')">
-                                            <img src="..\node_modules\bootstrap-icons\icons\chat-square-dots-fill.svg" width="auto" height="auto">
+                                            <input class="form-check-input" type="checkbox" name="permissions[]" value="comment" id="commentCheck" onClick="addPermission('comment')">
+                                            <i class="bi bi bi-chat-square-dots-fill text-info" style="font-size:20px;" width="auto" height="auto"></i>
                                         </label>
                                     </div>
 
                                     <div class="form-check  form-check-inline" data-bs-toggle="tooltip" title="Share">
                                         <label>
-                                            <input class="form-check-input" type="checkbox" name="permission-share" value="share" id="shareCheck" onClick="addPermission('share')">
-                                            <img src="..\node_modules\bootstrap-icons\icons\share-fill.svg" width="auto" height="auto">
+                                            <input class="form-check-input" type="checkbox" name="permissions[]" value="share" id="shareCheck" onClick="addPermission('share')">
+                                            <i class="bi bi-share text-warning " style="font-size:20px;" width="auto" height="auto"></i>
                                         </label>
                                     </div>
                                 </div>
@@ -164,14 +163,12 @@ date_default_timezone_set('Pacific/Auckland');
                         <div class="d-grid gap-2 mt-2">
                             <input type="submit" class="btn btn-primary btn-submit" name="submit" value="Submit">
                         </div>
-
-                        <div class="test-result float"></div>
                     </div>
                 </div>
         </div>
         <!------------------------ End of card ------------------------>
         </form>
-</div>
+    </div>
 </body>
 </div>
 
--- assign1/poststatusprocess.php
+++ assign1/poststatusprocess.php
@@ -1,11 +1,37 @@
 <?php
-
 include 'dbcon.php';
-Connect();
+$conn = Connect();
 
-if(isset($_POST["statuscode"])){
-    
-    
-    echo $_POST['statuscode'];
-    echo $_POST['date'];
+//to prevent SQL injection attacks, sql statements are in prepared statements.
+//the data from the forms are inserted into two tables. posts and post_permissions
+//Inserting data into posts table:
+$insert_cont = $conn->prepare('INSERT INTO posts VALUES (?,?,?, (SELECT visibility.id FROM visibility WHERE visibility.name = ? ))');
+$insert_cont->bind_param("ssss", $status_code, $status_content, $status_date, $status_visibility);
+
+//inserting data into post_permissions table:
+$insert_perm = $conn->prepare('INSERT INTO post_permissions (post_permissions.code, post_permissions.permission_id) VALUES (?, (SELECT permissions.id FROM permissions WHERE permissions.name = ?))');
+$insert_perm->bind_param("ss", $status_code, $p);
+
+
+if (($_POST['statuscode'] && $_POST['status'] && $_POST['date'] && $_POST['share-options'])) {
+    $status_code = "S" . $_POST['statuscode']; //status codes are prefixed with a capital S
+    $status_content = $_POST['status'];
+    $status_date =  $_POST['date'];
+    $status_visibility = $_POST['share-options'];
+
+    if (!isset($_POST['permissions'])) {
+        $status_permissions = "";
+    } else {
+        $status_permissions = $_POST['permissions'];
+        foreach ($status_permissions as $p) {
+            $insert_perm->execute();
+        }
+    }
+
+
+
+    $insert_cont->execute();
+
+    $insert_cont->close();
+    $insert_perm->close();
 }
